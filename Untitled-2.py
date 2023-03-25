6783556c96335c7c383daacae2a40cdd036ba347
Hayanne <121581813+HayDantas@users.noreply.github.com>
2023-03-25 20:17:42 -0300

    Add files via upload
    É uma aplicação da sequência de Fibonacci, no qual essa sequência se da pelos primeiros dois primeiro números como 0 e 1, e o restante é a somatória dos seus antecessores.
    Na primeira parte do código coloquei para fazer uma verificação do número escolhido, para sabermos se ele é desta sequência. Caso contrário, ele verificaria como 'Falso'.
    A estrutura de loop calcula a sequência de Fibonacci até que o último número seja maior ou igual a numero

---
+++ Untitled-2.py
@@ -0,0 +1,23 @@
+def verificacao_fibonacci(n):
+    a,b = 0, 1
+    while b < n :
+        a, b = b , a + b 
+    if b == n:
+        return True
+    else: 
+        return False
+
+
+numero = int (input("Digite um numero: "))
+sequencia_fibonacci = [0,1]
+
+while sequencia_fibonacci [-1] < numero: 
+    sequencia_fibonacci.append(sequencia_fibonacci[-1] + sequencia_fibonacci[-2])
+
+if numero in sequencia_fibonacci:
+    print ("{numero} faz parte da sequencia de fibonacci")
+
+else:
+    print("{numero} não faz parte da sequencia de fibonacci")
+ 
+     
